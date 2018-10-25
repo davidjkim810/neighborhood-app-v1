944eb35d16f273883e918efe644cb568e807cc81
David Kim <davidjkim810@gmail.com>
2018-10-25 11:00:25 -0400

    create session methods in app controller

--- neighborhood-api/app/controllers/api/sessions_controller.rb
+++ neighborhood-api/app/controllers/api/sessions_controller.rb
@@ -4,6 +4,10 @@ class Api::SessionsController < ApplicationController
   end
 
   def destroy
+    @user = current_user
+    if @user
+      logout
+      render json: {}
   end
 
 end
--- neighborhood-api/app/controllers/application_controller.rb
+++ neighborhood-api/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def current_user
+  end
+
 end
